34514e2a366c86a72b03b2aee823d26074ecfd4e
keskax <kevinstevenxyz_123@hotmail.com>
2023-09-05 01:49:28 -0500

    ok

---
+++ client/src/constants.js
@@ -0,0 +1,85 @@
+export const techStack = [
+  {
+    name: "Javascript",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "React",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+  },
+  {
+    name: "NodeJS",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+  },
+  {
+    name: "Express",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+  },
+
+  {
+    name: "Postman",
+    link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+  },
+
+  {
+    name: "HTML",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+  },
+  {
+    name: "Bootstrap",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+  },
+  {
+    name: "Redux",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+  },
+
+  {
+    name: "Git",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+  },
+  {
+    name: "Postgres",
+    link: "https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg",
+  },
+  {
+    name: "MaterialUI",
+    link: "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/materialui/materialui-original.svg",
+  },
+];
+
+export const contactLinks = [
+  {
+    name: "Github",
+    url: "https://img.icons8.com/doodle/40/000000/github--v1.png",
+    link: "https://github.com/Keskax",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://img.icons8.com/doodle/40/000000/linkedin--v2.png",
+    link: "https://www.linkedin.com/in/kevin-cabello-duarte/",
+  },
+];
+
+export const serviceData = [
+  {
+    name: "Web Application Development",
+    desc: "Designing and developing responsive websites that not only function flawlessly but also provide an engaging user experience. My proficiency in HTML5, CSS3, and JavaScript, coupled with hands-on experience in frameworks like React.js, allows me to create visually appealing and interactive web applications.",
+    img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png",
+  },
+  {
+    name: "Frontend Development",
+    desc: `Designing and developing responsive websites that not only function flawlessly but also provide an engaging user experience. My proficiency in HTML5, CSS3, and JavaScript, coupled with hands-on experience in frameworks like React.js, allows me to create visually appealing and interactive web applications.`,
+    img: "https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png",
+  },
+
+  {
+    name: "Backend Development",
+    desc: `Building the engine that drives web applications. With knowledge of Node.js, I develop strong server-side logic that supports the interface. I have experience in database management ensuring efficient data handling and retrieval.`,
+    img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png",
+  },
+];
---
+++ client/src/views/About.jsx
@@ -0,0 +1,94 @@
+import React, { useContext } from "react";
+import { techStack } from "../constants";
+import { ThemeContext } from "../themeProvider";
+import { motion } from "framer-motion";
+
+const About = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div id="about" className={darkMode === true ? "bg-white" : "bg-gray-900"}>
+      <div
+        className="max-w-7xl mx-auto x-4 sm:px-6 lg:px-8 px-4 md:mt-0 pt-24 pb-12"
+        data-section="about"
+        data-value="message_1"
+      >
+        <h2
+          className={
+            darkMode
+              ? "text-5xl font-bold px-4 md:px-0 text-center"
+              : "text-5xl font-bold px-4 md:px-0 text-center text-white"
+          }
+        >
+          About Me
+        </h2>
+        <div>
+          <motion.div>
+            <h4
+              className="mt-12 text-3xl font-semibold text-blue-500"
+              data-section="about"
+              data-value="message_2"
+            >
+              A bit about me
+            </h4>
+            <p
+              className={
+                darkMode
+                  ? "mt-4 text-xl text-justify text-gray-500"
+                  : "mt-4 text-xl text-justify text-white"
+              }
+            >
+              I'm a highly motivated and passionate Full Stack web developer
+              with a background in mining engineering. With experience in HTML5,
+              CSS3 and JavaScript, along with knowledge of frameworks like
+              React.js. In addition, I have command of agile methodologies, GIT
+              and experience in Back-End development with Node.js and relational
+              databases. With an intermediate level of English, I am looking for
+              an opportunity to apply my skills and knowledge in a professional
+              environment.
+            </p>
+          </motion.div>
+          <motion.div>
+            <h4 className="mt-12 text-3xl font-semibold text-blue-500">
+              Technologies and Tools
+            </h4>
+            <p
+              className={
+                darkMode
+                  ? "mt-4 text-xl text-justify text-gray-500"
+                  : "mt-4 text-xl text-justify text-white"
+              }
+            >
+              Using a combination of cutting-edge technologies and trusted open
+              source software, I create high-performance websites and apps
+            </p>
+          </motion.div>
+          <motion.div className="flex flex-wrap mt-8 flex flex-wrap justify-between ">
+            {techStack.map((el, index) => (
+              <motion.div
+                initial="hidden"
+                whileInView={"visible"}
+                variants={{
+                  visible: {
+                    y: 0,
+                    opacity: 1,
+                    transition: {
+                      type: "spring",
+                    },
+                  },
+                  hidden: { opacity: 1, y: 80 },
+                }}
+                className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40"
+              >
+                <img alt="" src={el.link} className="w-12" />
+                <h4 className="text-md ml-4">{el.name}</h4>
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
